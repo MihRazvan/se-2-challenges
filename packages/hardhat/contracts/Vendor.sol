9e3e10754af788ef0b1d85ce3e99cd8d436e0772
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-08-16 17:12:13 +0300

    initial functions draft

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,17 +5,26 @@ pragma solidity 0.8.4; //Do not change the solidity version as it negativly impa
 import "./YourToken.sol";
 
 contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	// event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+	YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+	}
+
+	// ToDo: create a payable buyTokens() function:
+	function buyTokens() public payable {
+    yourToken.transfer(address(this), msg.sender, amount???);
   }
 
-  // ToDo: create a payable buyTokens() function:
+	// ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public {
+    address(this).transfer(msg.sender, value: adress(this).balance);
+  }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-  // ToDo: create a sellTokens(uint256 _amount) function:
+	// ToDo: create a sellTokens(uint256 _amount) function:
+  function sellTokens(uint256 _amount) public {
+    msg.sender.transfer(adress(this), value: _amount);
+  }
 }
